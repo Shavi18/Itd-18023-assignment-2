1f8b1dbc8cb0804818b7392d82c5d8f9708ea3a2
Shavi18 <43333007+Shavi18@users.noreply.github.com>
2018-11-13 21:55:22 +0530

    Add files via upload

---
+++ Question 2/Question 2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace A2sahilpreetsinghP2
+{
+    class Program
+    {
+        static double volume(double r)
+        {
+            double ert = 4 / 3 * 3.14 * r * r * r;
+            Console.WriteLine("volume of sphere = " + ert);
+            return ert;
+        }
+        static double volume(double radi, double h)
+        {
+            double bc = 3.14 * radi * radi * h;
+            Console.WriteLine("volume of cylinder = " + bc);
+            return bc;
+        }
+        static double volume(double l, double w, double he)
+        {
+            double pq = l * w * he;
+            Console.WriteLine("volume of rectangular prism = " + pq);
+            return pq;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("ENTER :");
+            Console.WriteLine("1.Volume of sphere");
+            Console.WriteLine("2.Volume of cylinder");
+            Console.WriteLine("3.Volume of rectangular prism");
+            string cc = Console.ReadLine();
+            int ch = int.Parse(cc);
+
+            switch (ch)
+            {
+                case 1:
+                    Console.WriteLine("volume of sphere");
+                    Console.WriteLine("enter radius:");
+                    string rs = Console.ReadLine();
+                    double po = double.Parse(rs);
+                    volume(po);
+                    break;
+                case 2:
+                    Console.WriteLine("volume of cylinder");
+                    Console.WriteLine("enter radius:");
+                    string r = Console.ReadLine();
+                    double rad = double.Parse(r);
+                    Console.WriteLine("enter height:");
+                    string h = Console.ReadLine();
+                    double heit = double.Parse(h);
+                    volume(rad, heit);
+                    break;
+                case 3:
+                    Console.WriteLine("volume of rectangular prism");
+                    Console.WriteLine("enter lenght:");
+                    string l = Console.ReadLine();
+                    double len = double.Parse(l);
+                    Console.WriteLine("enter width:");
+                    string w = Console.ReadLine();
+                    double wid = double.Parse(w);
+                    Console.WriteLine("enter height:");
+                    string H = Console.ReadLine();
+                    double k = double.Parse(H);
+                    volume(len, wid, k);
+                    break;
+                default:
+                    Console.WriteLine("invalid entry");
+                    break;
+            }
+        }
+    }
+}
---
+++ question1/question1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             double t, reg, tax;
+            Console.WriteLine("Enter Student's age ");
+            string ab = Console.ReadLine();
+            int y = int.Parse(ab);
+            Console.WriteLine("International Student (or not) ");
+            string x = Console.ReadLine();
+            Console.WriteLine("Registration Semester ");
+            string smtr = Console.ReadLine();
+
+            if (x == "not")
+            {
+                if (y <= 18)
+                {
+                    t = 300 + (300 * 0.13);
+                    Console.WriteLine("Base Tution " + t);
+                }
+                else if (y <= 49)
+                {
+                    t = 500 + (500 * 0.13);
+                    Console.WriteLine("Base Tution " + t);
+                }
+                else
+                {
+                    t = 400 + (400 * 0.13);
+                    Console.WriteLine("Base Tution " + t);
+                }
+            }
+            else if (x == "yes")
+            {
+                if (y <= 18)
+                {
+                    t = 400 + (400 * 0.13);
+                    Console.WriteLine("Base Tution " + t);
+                }
+                else if (y <= 49)
+                {
+                    t = 600 + (600 * 0.13);
+                    Console.WriteLine("Base Tution " + t);
+                }
+                else
+                {
+                    t = 500 + (500 * 0.13);
+                    Console.WriteLine("Base Tution " + t);
+                }
+
+            }
+            switch (smtr)
+            {
+                case "fall":
+
+                    reg = 250 + (250 * 0.13);
+                    t = 500 + (500 * 0.13);
+                    Console.WriteLine("Registration Fee " + reg);
+                    tax = 250 * 0.13;
+                    Console.WriteLine("HST " + tax);
+                    double total = reg+t;
+                    Console.WriteLine("TOTAL:" + total);
+                    break;
+
+                case "winter":
+
+                    reg = 250 + (250 * 0.13);
+                    t = 500 + (500 * 0.13);
+                    Console.WriteLine("Registration Fee " + reg);
+                    tax = 220 * 0.13;
+                    Console.WriteLine("HST " + tax);
+                    total = reg+t;
+                    Console.WriteLine("TOTAL:" + total);
+                    break;
+
+                case "summer":
+
+                    reg = 150 + (150 * 0.13);
+                    t = 500 + (500 * 0.13);
+                    Console.WriteLine("Registration Fee " + reg);
+                    tax = 150 * 0.13;
+                    Console.WriteLine("HST " + tax);
+                    total = reg+t;
+                    Console.WriteLine("TOTAL:" + total);
+                    break;
+
+                default:
+
+                    Console.WriteLine("Invalid Entery");
+                    break;
+                }
+        }
+    }
+}
